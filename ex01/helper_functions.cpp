8a795118f6d2703f80d3d4bf164dfca1494cda1b
Zakaria El Mountassir <zael-mou@c1r1p5.1337.ma>
2025-10-24 08:58:08 +0100

    restore

---
+++ ex01/helper_functions.cpp
@@ -0,0 +1,134 @@
+#include "phonebook.hpp"
+
+int	ft_atoi(std::string nptr)
+{
+	int	i;
+	int	r;
+	int	sign;
+
+	i = 0;
+	r = 0;
+	sign = 1;
+	if ((nptr[i] == '-') || (nptr[i] == '+'))
+	{
+		if (nptr[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		r = r * 10 + nptr[i] - '0';
+        if (r * sign < INT_MIN || r > INT_MAX)
+            return (-1);
+		i++;
+	}
+	return (r * sign);
+}
+
+int count_spaces(std::string str, int start)
+{
+    int i;
+
+    if (!start)
+    {
+        i = 0;
+        while (str[i] && str[i] == ' ')
+            i++;
+        return (i);
+    }
+    else
+    {
+        i = str.length() - 1;
+        while (i != -1 && str[i] == ' ')
+            i--;
+        return (str.length() - 1 - i);
+    }
+    return 0;
+}
+
+int full_digits(std::string phone_number)
+{
+    int i = 0;
+    while (phone_number[i])
+    {
+        if (!std::isdigit(phone_number[i]))
+            return 0;
+        i++;
+    }
+    return 1;
+}
+
+void    long_string(std::string str, int pipe)
+{
+    std::cout << str.substr(0, 9);   
+    std::cout << '.';
+    if (pipe)
+        std::cout << '|';
+}
+
+void    short_string(std::string str, int pipe)
+{
+    int to_print = str.length() - 10;
+    if (to_print < 0)
+        to_print *= -1;
+    while (to_print > 0)
+    {
+        to_print--;
+        std::cout << ' ';
+    }
+    std::cout << str;
+    if (pipe)
+        std::cout << '|';
+}
+
+void    more_or_less(std::string str, int pipe)
+{
+        if (str.length() > 10)
+            long_string(str, pipe);
+        else
+            short_string(str, pipe);
+}
+
+std::string cut_spaces(std::string tmp)
+{
+    std::string new_tmp = tmp.substr(count_spaces(tmp, 0), tmp.length() - count_spaces(tmp, 1));
+    return (new_tmp);
+}
+
+std::string get_input(std::string type, int *cntrl_d)
+{
+    int empty = 1;
+    std::string tmp;
+    while (empty)
+    {
+        std::cout << type;
+        if(!std::getline(std::cin, tmp))
+        {
+            *cntrl_d = 1;
+            break;
+        }
+        tmp = cut_spaces(tmp);
+        if (!tmp[0])
+            std::cerr << RED "invalid input!" RESET "\n";
+        else
+            empty = 0;
+    }
+    return tmp;
+}
+
+std::string check_number(int *cntrl_d, std::string message)
+{
+    std::string phone_number;
+    int not_number = 1;
+    while (not_number)
+    {
+        phone_number = get_input(message, cntrl_d);
+        if (*cntrl_d)
+            break;
+        if (!full_digits(phone_number))
+            std::cerr << RED "There is non int in the number!" RESET "\n";
+        else
+            not_number = 0;
+    }
+    return phone_number;
+}
---
+++ ex01/phonebook.hpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <limits.h>
+
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#define RED "\033[1;31m"
+#define RESET "\033[0m"
+#define GREEN "\033[1;32m"
+#define DEEP_BLUE "\033[1;34m"
+#define SKY_BLUE "\033[1;36m"
+
+class Contact {
+    private:
+        int         cntrl_d;
+        int         index;
+        std::string first_name;
+        std::string last_name;
+        std::string nickname;
+        std::string phone_number;
+        std::string darkest_secret;
+
+    public:
+        void    add(int index);
+        void    print_data(Contact data);
+        void    show_all_contact();
+        void    print_contact_data();
+        int     get_flag(int cntrl_d);
+        int     get_index(int index);
+};
+
+class PhoneBook {
+    public:
+        int     enter_commande();
+        void    get_contacts(Contact contact, int index);
+        void    get_input(std::string input);
+        void    handle_commands();
+        void    search(int number, int index);
+
+    private:
+        std::string input;
+        Contact contact[8];
+};
+
+int	ft_atoi(std::string nptr);
+int count_spaces(std::string str, int start);
+int full_digits(std::string phone_number);
+void    long_string(std::string str, int pipe);
+void    short_string(std::string str, int pipe);
+void    more_or_less(std::string str, int pipe);
+std::string cut_spaces(std::string tmp);
+std::string get_input(std::string type, int *cntrl_d);
+std::string check_number(int *cntrl_d, std::string message);
+
+#endif
